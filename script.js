a1b99190f55c697780924a91cf498d131e23afd4
Chanel Devlin <cldev66@gmail.com>
2026-04-18 02:01:42 -0400

    Your changes

---
+++ script.js
@@ -0,0 +1,10 @@
+// Check if the page is loaded
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("Storefront is ready.");
+
+  // Example: Alert user when they add an item (Optional)
+  /* document.addEventListener('snipcart.events.item.adding', (item) => {
+        console.log(`${item.name} is being added to cart`);
+    }); 
+    */
+});
